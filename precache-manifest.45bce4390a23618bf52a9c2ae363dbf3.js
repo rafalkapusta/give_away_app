0fd8dd11e19b666871c5de7fde0619a01f5119a6
rafalkapusta <rfkapusta@gmail.com>
2019-12-17 16:03:56 +0100

    Updates

---
+++ precache-manifest.45bce4390a23618bf52a9c2ae363dbf3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc23cce31b239f6a0ed848580994686",
+    "url": "/give_away_app/index.html"
+  },
+  {
+    "revision": "af8d7ead163be352a267",
+    "url": "/give_away_app/static/css/main.58db5697.chunk.css"
+  },
+  {
+    "revision": "79a26380a062767dbb2f",
+    "url": "/give_away_app/static/js/2.f3b6e3e2.chunk.js"
+  },
+  {
+    "revision": "af8d7ead163be352a267",
+    "url": "/give_away_app/static/js/main.f11d5b5d.chunk.js"
+  },
+  {
+    "revision": "b5d1ef4bc2344eac74c4",
+    "url": "/give_away_app/static/js/runtime-main.333e0e0f.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/give_away_app/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/give_away_app/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/give_away_app/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/give_away_app/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/give_away_app/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/give_away_app/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/give_away_app/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/give_away_app/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/give_away_app/static/media/Signature.e54cfcfc.svg"
+  }
+]);
